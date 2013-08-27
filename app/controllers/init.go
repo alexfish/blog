7b0a5d5e22928cde5e03753e15336bce4becfe86
Alex Fish <alex@alexefish.com>
2013-08-27 22:57:51 +0100

    Updating Styling and html layout
    Updating general css stylings and blog layout to be more pleasing on the
    eyes.

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -1,7 +1,15 @@
 package controllers
 
-import "github.com/jgraham909/revmgo"
+import (
+  "github.com/jgraham909/revmgo"
+  "github.com/robfig/revel"
+  "time"
+)
 
 func init() {
     revmgo.ControllerInit()
+    revel.TemplateFuncs["year"] = func() string {
+      t := time.Now()
+      return t.Format("2006")
+    }
 }
